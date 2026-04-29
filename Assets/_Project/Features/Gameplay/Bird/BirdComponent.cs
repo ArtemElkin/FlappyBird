15683078cf2f6177d184c93ed62add8e395a53ab
ArtemElkin <elkin.artem@protonmail.com>
2026-04-30 01:58:24 +0300

    Add rigidbody to bird

--- Assets/_Project/Features/Gameplay/Bird/BirdComponent.cs
+++ Assets/_Project/Features/Gameplay/Bird/BirdComponent.cs
@@ -7,17 +7,19 @@ namespace _Project.Features.Gameplay.Bird
     public class BirdComponent : MonoBehaviour
     {
         private BirdMovementController _movementController;
+        private Rigidbody2D _rb;
         
         [Inject]
         private void Construct(BirdMovementController movementController)
         {
             _movementController = movementController;
+            _rb = GetComponent<Rigidbody2D>();
         }
 
         private void FixedUpdate()
         {
             Vector3 newPos = _movementController.CalculateNewLocalPosition(transform.localPosition, Time.fixedDeltaTime);
-            transform.localPosition = newPos;
+            _rb.MovePosition(newPos);
         }
 
         private void OnDestroy()
