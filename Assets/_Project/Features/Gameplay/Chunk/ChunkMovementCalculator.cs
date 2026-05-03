b0c3cba49179f9bec851ab5225613c809f846a12
ArtemElkin <elkin.artem@protonmail.com>
2026-05-03 22:36:41 +0300

    Rename ChunkMovementController

---
+++ Assets/_Project/Features/Gameplay/Chunk/ChunkMovementCalculator.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace _Project.Features.Gameplay.Chunk
+{
+    public class ChunkMovementCalculator
+    {
+        public Vector3 CalculateNewPosition(Vector3 currentPos, float speed, float fixedDeltaTime)
+        {
+            currentPos.x -= speed * fixedDeltaTime;
+            return currentPos;
+        }
+    }
+}
