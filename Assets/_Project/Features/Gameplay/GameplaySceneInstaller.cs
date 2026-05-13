660779786b2bd0a33112d9ea001fc0d45306077c
ArtemElkin <elkin.artem@protonmail.com>
2026-05-13 12:23:10 +0300

    Move BindGameplayAdsController to GameplaySceneInstaller

--- Assets/_Project/Features/Gameplay/GameplaySceneInstaller.cs
+++ Assets/_Project/Features/Gameplay/GameplaySceneInstaller.cs
@@ -1,4 +1,5 @@
 using _Project.Core.Tools;
+using _Project.Features.Gameplay.Ads;
 using Zenject;
 
 namespace _Project.Features.Gameplay
@@ -8,12 +9,20 @@ namespace _Project.Features.Gameplay
         public override void InstallBindings()
         {
             BindScreenBoundsCalculator();
+            BindGameplayAdsController();
         }
         
         private void BindScreenBoundsCalculator()
         {
             Container
                 .Bind<ScreenBoundsCalculator>()
+                .AsSingle();
+        }
+        
+        private void BindGameplayAdsController()
+        {
+            Container
+                .BindInterfacesAndSelfTo<GameplayAdsController>()
                 .AsSingle();
         }
     }
